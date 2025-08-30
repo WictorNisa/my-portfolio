4363c18ebfa21e738bbadaa83e6a832d5264bfc5
WictorNisa <wictornisa@hotmail.com>
2025-08-30 15:33:22 +0200

    feat(SectionBreaker): Add section breaker

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
+import SectionBreaker from "./components/SectionBreaker/SectionBreaker";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <SectionBreaker />
       <Projects />
       <Footer />
     </div>
---
+++ src/components/SectionBreaker/SectionBreaker.tsx
@@ -0,0 +1,49 @@
+import { motion } from "motion/react";
+import styles from "./SectionBreaker.module.css";
+
+const SectionBreaker = () => {
+  return (
+    <div className={styles.sectionBreakerContainer}>
+      <motion.div
+        className={styles.sectionBreakerInnerContainer}
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ once: true, amount: 0.3 }}
+      >
+        <motion.h3
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          viewport={{ once: true }}
+        >
+          Explore My Creative Journey
+        </motion.h3>
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.4 }}
+          viewport={{ once: true }}
+        >
+          Did you know I have a webpage where I explore my creative and visual
+          ideas?
+        </motion.p>
+        <motion.button
+          initial={{ opacity: 0, scale: 0.8 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5, delay: 0.6 }}
+          viewport={{ once: true }}
+          whileHover={{
+            scale: 1.05,
+            transition: { duration: 0.2 },
+          }}
+          whileTap={{ scale: 0.95 }}
+        >
+          View page
+        </motion.button>
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionBreaker;
